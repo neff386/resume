b951526a50b89e3cde159deb8443e999166539bb
David Turner <dturner@DTurner-Macbook-18.local>
2018-03-20 11:07:32 -0400

    updating references

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 var express = require('express');
 var app = express();
-app.use(express.static('/'));
+app.use(express.static('public'));
 
 
 app.listen(PORT, function(err) {
