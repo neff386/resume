74456b63b916f7bfe48e4e4377afad2c6e2dbfce
David Turner <dturner@DTurner-Macbook-18.local>
2018-03-20 10:34:10 -0400

    deployment changes

---
+++ js/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+var express = require('express');
+var app = express();
+app.use(express.static('/'));
+
+
+app.listen(PORT, function(err) {
+
+    console.log('running');
+    
+});
