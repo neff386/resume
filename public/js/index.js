de8b7c1f831cacf690871944efd44f5b863197b3
David Turner <dturner@DTurner-Macbook-18.local>
2018-10-02 11:35:25 -0400

    update 10 02

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 var express = require('express');
+var path = require('path');
 var app = express();
 app.use(express.static('public'));
 
+
+app.get('/resume', function (req, res) {
+    res.download(path.resolve('docs/Resume_of_David_Turner.docx'), 'Resume Of David Turner');
+});
 
 app.listen(PORT, function(err) {
 
